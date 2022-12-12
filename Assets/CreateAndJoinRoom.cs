0fd463e75e50904938b40f467cfae7da8d7e0558
shrikar bomirasipet <Shrikar2606@gmail.com>
2022-12-13 09:50:56 +1100

    added menus

---
+++ Assets/CreateAndJoinRoom.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
+{
+    public TMPro.TMP_InputField createInput;
+    public TMPro.TMP_InputField joinInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("MainGame");
+    }
+
+}
